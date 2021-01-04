ac888d5eefc6b63bc3b849f819431b969149c7c4
Mohib Ismail <imohib168@gmail.com>
2021-01-04 14:13:29 +0500

    Home page Complete

--- js/index/accountName.js
+++ js/index/accountName.js
@@ -19,3 +19,26 @@ if (userName) {
 }
 
 showUser.innerHTML = ` <strong> Hello, ${userName ? userName : "Anonymous User"} </strong> `;
+
+
+// var userName = localStorage.getItem('name');
+// var showUser = document.getElementById("showUserName");
+
+// if (userName) {
+//     var login = document.getElementById("login");
+//     login.innerHTML = "<a>Signout</a>";
+//     login.addEventListener('click', () => {
+//         localStorage.clear();
+//         login.innerHTML = "<a>Login</a>";
+//         login.addEventListener('click', () => {
+//             location.href = "../../../login.html"
+//         })
+//     })
+// } else {
+//     var login = document.getElementById("login");
+//     login.addEventListener('click', () => {
+//         location.href = "../../../login.html"
+//     })
+// }
+
+// showUser.innerHTML = ` <strong> Hello, ${userName ? userName : "Anonymous User"} </strong> `;
